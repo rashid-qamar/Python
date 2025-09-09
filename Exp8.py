4b3f5a0eb2f42ab9596f91079cf0712eaef4098d
Rashid Qamar.. <rashidqamar841419@gmail.com>
2025-09-09 19:33:35 +0530

    Add exp 8

--- Exp8.py
+++ Exp8.py
@@ -5,16 +5,21 @@ print ('A =', bin(a))
 print ('B =', bin(b))
 
 And = a & b
-print ("result for AND is: ", bin(And))
+print ("result for AND is ", And, ": ", bin(And),"\n")
 
 Or = a | b
-print ("result for OR is: ", bin(Or))
+print ("result for OR is ", Or, ": " , bin(Or),"\n")
 
 xor = a ^ b
-print ("result for XOR is: ", bin(xor))
+print ("result for XOR is ", xor, ": ", bin(xor),"\n")
 
 com1 = ~a
 com2 = ~b
-print ("Complement of A: ", com1, ": " bin(com1))
-print ("Complement of B: ", com2, ": " bin(com2))
+print ("Complement of A ", com1, ": ", bin(com1))
+print ("Complement of B ", com2, ": ", bin(com2),"\n")
 
+lshift = a<<2;
+print ("Left shift of A", lshift, ": ", bin(lshift),"\n")
+
+rshift = a>>2;
+print ("Right shift of A ", rshift, ": ", bin(rshift),"\n")
--- README.md
+++ README.md
@@ -4,4 +4,8 @@
 ## Code 2. Demonstrate List
 ## Code 3. Demonstrate Tuples
 ## Code 4. Demonstrate Dictionary
-## Code 5. Demonstrate Operators
+## Code 5. Demonstrate Arihtmetic Operators
+## Code 6. Demonstrate Comparison Operation
+## Code 7. Demonstrate Assigment Operators
+## Code 8. Demonstrate Bitwise Operators
+## Code 9. Demonstrate Membership Operators
